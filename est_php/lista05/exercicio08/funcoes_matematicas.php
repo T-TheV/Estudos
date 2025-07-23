35644b12d5a87bfc5c7627f0caebb4638fad2994
T-TheV <djardim322@gmail.com>
2025-07-23 12:32:40 -0300

    add: Adicionadas funções para calcular quadrado e média, e criado arquivo principal para testes

---
+++ est_php/lista05/exercicio08/funcoes_matematicas.php
@@ -0,0 +1,12 @@
+<?php
+
+function calcularQuadrado($numero) {
+    $quadrado = $numero * $numero;
+    return $quadrado;
+    echo $quadrado;
+}
+
+function calcularMedia($nota1, $nota2) {
+    $media = ($nota1 + $nota2) / 2;
+    return $media;
+}
---
+++ est_php/lista05/exercicio08/principal.php
@@ -0,0 +1,5 @@
+<?php
+require 'funcoes_matematicas.php';
+
+echo "O quadrado de 5 é: " . calcularQuadrado(5) . "<br>";
+echo "A média entre 7 e 8 é: " . calcularMedia(7, 8) . "<br>";
